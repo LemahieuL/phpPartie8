9b4d263d72721297af53f502e8432004f7d34d12
Luc <luc.lemahieu@laposte.net>
2018-11-22 16:41:23 +0100

    préparation des exos

---
+++ exercice5/index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>Exercice 5 php Partie 8</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
